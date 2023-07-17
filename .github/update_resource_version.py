6dd6d4dbc48dcc1aa133c52bac000db4ecf7ba45
MistEO <mistereo@hotmail.com>
2023-07-17 23:55:28 +0800

    fix: 修复版本号更新脚本

--- .github/update_resource_version.py
+++ .github/update_resource_version.py
@@ -1,12 +1,12 @@
-import os
+import sys
 import json
 
-if len(os.argv) < 3:
+if len(sys.argv) < 3:
     print("Usage: python update_resource_version.py <properties_file> <version>")
     exit(1)
 
-properties_file = os.argv[1]
-version = os.argv[2]
+properties_file = sys.argv[1]
+version = sys.argv[2]
 
 with open(properties_file, "r") as f:
     data = json.load(f)
