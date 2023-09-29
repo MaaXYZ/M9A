b65f1cd859eaa40e6ac3775af04348ad49d6c749
MistEO <mistereo@hotmail.com>
2023-09-30 01:27:37 +0800

    chore: 使用MaaCommonAssets的OCR模型

--- cmake/assets.cmake
+++ cmake/assets.cmake
@@ -2,8 +2,9 @@ set(maa_assets_target ${CMAKE_BINARY_DIR})
 
 add_custom_command(OUTPUT ${maa_assets_target}/resource
     COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/assets/resource ${maa_assets_target}/resource
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/assets/MaaCommonAssets/OCR/ppocr_v4/zh_cn ${maa_assets_target}/resource/model/ocr
 )
 add_custom_target(AssetsResource ALL DEPENDS ${maa_assets_target}/resource)
 set_property(TARGET AssetsResource APPEND PROPERTY ADDITIONAL_CLEAN_FILES ${maa_assets_target}/resource)
 set_target_properties(AssetsResource PROPERTIES FOLDER Assets)
-install(DIRECTORY ${PROJECT_SOURCE_DIR}/assets/resource DESTINATION .)
+install(DIRECTORY ${maa_assets_target}/resource DESTINATION .)
