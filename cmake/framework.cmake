f46ac92e1f20ae3f4e3f248b04ab7d43d1180ab1
MistEO <mistereo@hotmail.com>
2023-11-29 00:53:05 +0800

    fix: 修正linux下打包错误

--- cmake/framework.cmake
+++ cmake/framework.cmake
@@ -1,7 +1,7 @@
 install(DIRECTORY "${CMAKE_SOURCE_DIR}/deps/bin/" DESTINATION .
-    PATTERN "MaaDbgControlUnit*" EXCLUDE
-    PATTERN "MaaThriftControlUnit*" EXCLUDE
-    PATTERN "MaaWin32ControlUnit*" EXCLUDE
-    PATTERN "MaaRpc*" EXCLUDE
+    PATTERN "*MaaDbgControlUnit*" EXCLUDE
+    PATTERN "*MaaThriftControlUnit*" EXCLUDE
+    PATTERN "*MaaWin32ControlUnit*" EXCLUDE
+    PATTERN "*MaaRpc*" EXCLUDE
 )
 install(DIRECTORY "${CMAKE_SOURCE_DIR}/deps/share/MaaAgentBinary" DESTINATION .)
