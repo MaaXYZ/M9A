115d4c5ff35008a1d6a5dc06333060a6c2c2ac08
MistEO <mistereo@hotmail.com>
2024-02-14 02:28:12 +0800

    chore: 更新CI

--- install.py
+++ install.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+
 import shutil
+import sys
+import json
 
 working_dir = Path(__file__).parent
 install_path = working_dir / Path("install")
+version = len(sys.argv) > 1 and sys.argv[1] or "v0.0.1"
 
 if __name__ == "__main__":
     shutil.copytree(
@@ -36,3 +40,19 @@ if __name__ == "__main__":
         install_path / "resource" / "base" / "model" / "ocr",
         dirs_exist_ok=True,
     )
+    shutil.copy2(
+        working_dir / "README.md",
+        install_path,
+    )
+    shutil.copy2(
+        working_dir / "LICENSE",
+        install_path,
+    )
+
+    with open(install_path / "interface.json", "r", encoding="utf-8") as f:
+        interface = json.load(f)
+
+    interface["version"] = version
+
+    with open(install_path / "interface.json", "w", encoding="utf-8") as f:
+        json.dump(interface, f, ensure_ascii=False, indent=4)
