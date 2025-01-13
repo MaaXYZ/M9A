78c50371cfe2c7c16a4b284e2aa08073a23f5f69
MistEO <mistereo@hotmail.com>
2025-01-14 01:57:26 +0800

    chore: 预适配 maafw v3

--- check_resource.py
+++ check_resource.py
@@ -14,8 +14,8 @@ def check(dirs: List[Path]) -> bool:
 
     for dir in dirs:
         print(f"Checking {dir}...")
-        status = resource.post_path(dir).wait().status()
-        if not status.succeeded():
+        status = resource.post_bundle(dir).wait().status
+        if not status.succeeded:
             print(f"Failed to check {dir}.")
             return False
 
