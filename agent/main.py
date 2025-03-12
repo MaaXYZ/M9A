325b5a184bddaa3100348cd32a833ed62b1a2c9c
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2025-03-12 17:40:56 +0800

    fix: 动态导入

--- agent/main.py
+++ agent/main.py
@@ -1,7 +1,13 @@
+import os
 import sys
 
 from maa.agent.agent_server import AgentServer
 from maa.toolkit import Toolkit
+
+# 将当前目录添加到路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
 
 from action import *
 
