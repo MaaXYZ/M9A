c67305842fe5ddb4367b99650171375eea33afa9
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2025-03-13 08:49:32 +0800

    chore: 整理文件结构

--- agent/main.py
+++ agent/main.py
@@ -9,7 +9,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
 
-from action import *
+from custom.action import *
 
 
 def main():
