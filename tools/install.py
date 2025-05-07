8ac755e9a5cdf4394901710a5af1ae04a6f2b635
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2025-05-07 18:10:05 +0800

    chore: 调试源代码不再需要install后才能调试

--- tools/install.py
+++ tools/install.py
@@ -82,15 +82,16 @@ def install_agent():
         dirs_exist_ok=True,
     )
 
+    with open(install_path / "interface.json", "r", encoding="utf-8") as f:
+        interface = json.load(f)
+
     if sys.platform.startswith("win"):
-
-        with open(install_path / "interface.json", "r", encoding="utf-8") as f:
-            interface = json.load(f)
-
         interface["agent"]["child_exec"] = r"{PROJECT_DIR}/python/python.exe"
 
-        with open(install_path / "interface.json", "w", encoding="utf-8") as f:
-            json.dump(interface, f, ensure_ascii=False, indent=4)
+    interface["agent"]["child_args"] = [r"{PROJECT_DIR}/agent/main.py", "-u"]
+
+    with open(install_path / "interface.json", "w", encoding="utf-8") as f:
+        json.dump(interface, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
